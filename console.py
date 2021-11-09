94ebd97aad710601346071352b1b24b59ff86261
Emediong Francis <emediongfrancis@gmail.com>
2021-11-09 17:08:09 +0100

    initialized tests folder.

--- console.py
+++ console.py
@@ -6,23 +6,23 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """
-    Starting the interpreter
+    Starting the interpreter.
     """
     prompt = "(hbnb) "
 
     def do_EOF(self, line):
-        """Exit with Ctr-D"""
+        """Exit with Ctrl-D."""
         print()
         return True
     
     def do_quit(self, line):
-        """Exit with quit"""
+        """Exit with `quit`."""
         print()
         return True
 
 
 def parse(line):
-    """Helper method to parse user typed input"""
+    """Helper method to parse user-typed input."""
     return tuple(line.split())
 
 
