06035a07fc060da7839dc7a2a7fe01962b7cc72b
Emediong Francis <emediongfrancis@gmail.com>
2021-11-10 08:56:13 +0100

    updated test imports for console.

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,6 +3,9 @@
     Unit tests for console.py
 """
 import unittest
-import console
+from console import HBNBCommand
 import io
 from datetime import datetime
+from os import path, remove
+from models.base_model import BaseModel
+from unittest.mock import patch
