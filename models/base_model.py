15e10a85de003f82d26cc367efba2585c6016a68
Collin-5 <collins380@gmail.com>
2021-11-10 12:54:20 +0300

    updating the constaructor

--- models/base_model.py
+++ models/base_model.py
@@ -17,24 +17,22 @@ class BaseModel:
         """
             Initializes an instance of BaseModel.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        if len(kwargs) > 0:
+        if kwargs:
             for key, val in kwargs.items():
-                if key == "created_at":
-                    self.created_at = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.updated_at = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                if "created_at" == key:
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "updated_at" == key:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "__class__" == key:
+                    pass
                 else:
                     setattr(self, key, val)
-
-                # not sure if this is the best way to do this, or
-                # the `setattr` function automatically updates the id
-                # based on initial instancing.
-                if self.id == None:
-                    self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
        
     def __str__(self):
@@ -51,16 +49,6 @@ class BaseModel:
         """
         self.updated_at = datetime.now()
 
-<<<<<<< HEAD
-"""
-Module BaseModel
-Parent of all classes
-"""
-
-
-class BaseModel():
-    pass
-=======
     def to_dict(self):
         """
             Returns a dictionary containing all keys/values 
@@ -71,4 +59,3 @@ class BaseModel():
         myDict["created_at"] = self.created_at.isoformat()
         myDict["updated_at"] = self.updated_at.isoformat()
         return myDict
->>>>>>> 06035a07fc060da7839dc7a2a7fe01962b7cc72b
