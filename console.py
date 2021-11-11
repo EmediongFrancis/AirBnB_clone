841225fa501dd5878d5a16ad535465aacb4bc249
Emediong Francis <emediongfrancis@gmail.com>
2021-11-11 22:59:52 +0100

    updated console.

--- console.py
+++ console.py
@@ -2,41 +2,135 @@
 """
 Building a command interpreter.
 """
-import cmd
 
+import cmd
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.__init__ import storage
 class HBNBCommand(cmd.Cmd):
     """
-    Starting the interpreter.
+    Command interpreter for the HBNB console.
     """
     prompt = "(hbnb) "
+    intro = "Welcome to the HBNB console for Collins & Francis!\n"
+    intro += "Enter 'help' to see a list of commands.\n"
+    
+
+    def do_quit(self, line):
+        """Quit command to exit the program."""
+        return True
 
     def do_EOF(self, line):
         """Exit with Ctrl-D."""
-        print()
         return True
-    
-    def do_quit(self, line):
-        """Exit with `quit`."""
-        print()
-        return True
+
+    def emptyline(self):
+        """Do nothing on empty input line."""
+        pass
+    def do_help(self, line):
+        """
+           List of commands.
+        """
+        print("\n".join(self.get_help_text().split("\n")[1:]))
 
     def do_create(self, line):
         """
-           Creates a new instance of BaseModel, saves it
-           to disk, and prints the id.
+            Creates a new instance of BaseModel, saves it
+            to disk, and prints the id.
         """
-        if line:
-            print("** class doesn't exist **")
-        else:
+        if not line:
             print("** class name missing **")
-        return False
-    
+        else:
+            try:
+                new_instance = eval(line)()
+                new_instance.save()
+                print(new_instance.id)
+            except:
+                print("** class doesn't exist **")
 
+    def do_show(self, line):
+        """
+            Prints the string representation of an instance
+            based on the class name and id.
+        """
+        if not line:
+            print("** class name missing **")
+        else:
+            try:
+                args = line.split()
+                if len(args) == 1:
+                    print("** instance id missing **")
+                if args[1] not in storage.all().keys():
+                    print("** no instance found **")
+                else:
+                    print(eval(args[0] + "." + args[1])())
+            except:
+                print("** class doesn't exist **")
 
-def parse(line):
-    """Helper method to parse user-typed input."""
-    return tuple(line.split())
+    def do_destroy(self, line):
+        """
+            Deletes an instance based on the class name and id.
+        """
+        if not line:
+            print("** class name missing **")
+        else:
+            try:
+                args = line.split()
+                if len(args) == 1:
+                    print("** instance id missing **")
+                else:
+                    storage = FileStorage()
+                    if args[1] not in storage.all().keys():
+                        print("** no instance found **")
+                    else:
+                        eval(args[0] + "." + args[1]).delete()
+                        storage.save()
+            except:
+                print("** class doesn't exist **")
 
+    def do_all(self, line):
+        """
+            Prints all string representation of all instances
+            based or not on the class name.
+        """
+        storage = FileStorage()
+        if not line:
+            print([str(i) for i in storage.all().values()])
+        else:
+            try:
+                print([str(i) for i in storage.all().values() if
+                       type(i).__name__ == line])
+            except:
+                print("** class doesn't exist **")
+
+    def do_update(self, line):
+        """
+            Updates an instance based on the class name and id
+            by adding or updating attribute
+        """
+        if not line:
+            print("** class name missing **")
+        else:
+            try:
+                args = line.split()
+                if len(args) == 1:
+                    print("** instance id missing **")
+                elif len(args) == 2:
+                    print("** attribute name missing **")
+                elif len(args) == 3:
+                    print("** value missing **")
+                else:
+                    if args[1] not in storage.all().keys():
+                        print("** no instance found **")
+                    else:
+                        eval(args[0] + "." + args[1]).update(args[2], args[3])
+                        storage.save()
+            except:
+                print("** class doesn't exist **")
+
+    def parse(line):
+        """Helper method to parse user-typed input."""
+        return tuple(line.split())
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
