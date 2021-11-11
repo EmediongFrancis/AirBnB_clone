2bee249c39c0b77d94c1ff756e8426ca2a5e0bcb
Emediong Francis <emediongfrancis@gmail.com>
2021-11-11 19:45:21 +0100

    added calls to methods in base_model.

--- models/base_model.py
+++ models/base_model.py
@@ -32,6 +32,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            storage.new(self)
           # (not yet) self.updated_at = datetime.now()
 
     def __str__(self):
@@ -49,7 +50,7 @@ class BaseModel:
         """
         self.updated_at = datetime.now()
         storage.save()
-        
+
 
     def to_dict(self):
         """
