1f3cebc8c3ae3a8825041fc4974fd68fa200247e
Emediong Francis <emediongfrancis@gmail.com>
2021-11-13 23:08:15 +0100

    updated user class.

--- models/user.py
+++ models/user.py
@@ -13,3 +13,9 @@ class User(BaseModel):
     password = ''
     first_name = ''
     last_name = ''
+
+    def __init__(self, *args, **kwargs):
+            """
+                Initializes a User instance.
+            """
+            super().__init__(*args, **kwargs)
