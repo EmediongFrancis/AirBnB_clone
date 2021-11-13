821ef407ffbcbf8e753df995a10538aa876ff28f
Emediong Francis <emediongfrancis@gmail.com>
2021-11-13 02:07:32 +0100

    updated FileStorage and console to handle new classes.

--- console.py
+++ console.py
@@ -7,6 +7,12 @@ from models.base_model import BaseModel
 from models import storage
 import cmd
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
     """
